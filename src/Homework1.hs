29fc280d2908aa09e0d99c3d86404658b2f3a9de
Zation <zation1@gmail.com>
2018-11-16 22:14:54 +0800

    add test

--- src/Homework1.hs
+++ src/Homework1.hs
@@ -33,16 +33,3 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
-
-main :: IO ()
-main = do
-  print (toDigitsRev 1234)
-  print (toDigits 1234)
-  print (toDigits 0)
-  print (toDigits (-1))
-  print (doubleEveryOther [1,2,3,4])
-  print (doubleEveryOther [1,2,3,4,5])
-  print (sumDigits [16, 1, 22, 3])
-  print (validate 4012888888881881)
-  print (validate 4012888888881882)
-  print (hanoi 3 "a" "b" "c")
--- test/Homework1Spec.hs
+++ test/Homework1Spec.hs
@@ -1,10 +1,27 @@
 module Homework1Spec (spec) where
 
-import Homework1
-import Test.Hspec
+import           Homework1
+import           Test.Hspec
 
 spec :: Spec
 spec = do
   describe "toDigits" $ do
+    it "should be right" $ toDigits 1234 `shouldBe` [1,2,3,4]
+    it "should handle 0" $ toDigits 0 `shouldBe` []
+  describe "toDigitsRev" $ do
+    it "should be right" $ toDigitsRev 1234 `shouldBe` [4,3,2,1]
+    it "should handle 0" $ toDigits 0 `shouldBe` []
+  describe "doubleEveryOther" $ do
+    it "should double odd array" $ doubleEveryOther [1,2,3,4] `shouldBe` [2,2,6,4]
+    it "should double even array" $ doubleEveryOther [1,2,3,4,5] `shouldBe` [1,4,3,8,5]
+    it "should not double empty array" $ doubleEveryOther [] `shouldBe` []
+  describe "sumDigits" $ do
+    it "should sum by digit" $ sumDigits [16,1,22,3] `shouldBe` 15
+    it "should sum empty array as 0" $ sumDigits [] `shouldBe` 0
+  describe "validate" $ do
     it "should be right" $ do
-      toDigits 1234 `shouldBe` [1,2,3,4]
+      validate 4012888888881881 `shouldBe` True
+      validate 4012888888881882 `shouldBe` False
+  describe "hanoi" $ do
+    it "should calculate steps" $ do
+      hanoi 3 "a" "b" "c" `shouldBe` [("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b")]
