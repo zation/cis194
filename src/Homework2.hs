d1e3c0e26b464cec3080cab0268669b68bb85df4
Zation <zation1@gmail.com>
2018-11-18 12:31:59 +0800

    Homework2 parse

--- src/Homework2.hs
+++ src/Homework2.hs
@@ -6,4 +6,8 @@ parseMessage :: String -> LogMessage
 parseMessage message = case words message of
   "E":severity:timeStamp:content -> LogMessage (Error (read severity::Int)) (read timeStamp::Int) (unwords content)
   "I":timeStamp:content -> LogMessage Info (read timeStamp::Int) (unwords content)
+  "W":timeStamp:content -> LogMessage Warning (read timeStamp::Int) (unwords content)
   content -> Unknown (unwords content)
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
--- test/Homework2Spec.hs
+++ test/Homework2Spec.hs
@@ -9,4 +9,9 @@ spec = do
   describe "parseMessage" $ do
     it "should parse error" $ parseMessage "E 2 562 help help" `shouldBe` LogMessage (Error 2) 562 "help help"
     it "should parse information" $ parseMessage  "I 29 la la la" `shouldBe` LogMessage Info 29 "la la la"
+    it "should parse warning" $ parseMessage  "W 11 bong bong" `shouldBe` LogMessage Warning 11 "bong bong"
     it "should parse unknown" $ parseMessage  "This is not in the right format" `shouldBe` Unknown "This is not in the right format"
+  describe "parse" $ do
+    it "should parse multiple lines" $ do
+      parse "E 2 562 help help\nI 29 la la la\nW 11 bong bong\nThis is not in the right format" `shouldBe`
+        [LogMessage (Error 2) 562 "help help", LogMessage Info 29 "la la la", LogMessage Warning 11 "bong bong", Unknown "This is not in the right format"]
